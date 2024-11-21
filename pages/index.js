ba6afc06b04dc58dfd6e5ca7c46199d148857dad
Zain-appiness <zain@appinessworld.com>
2024-11-21 15:29:36 +0530

    vercel.json

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
-import Game from "./_app"; // Make sure the path to Game.js is correct
+import Game from "./_app"; 
 
 export default function Home() {
   return (
     <div>
       <h1>Welcome to Tic-Tac-Toe</h1>
-      <Game />  {/* Render the Game component here */}
+      <Game />  
     </div>
   );
 }
